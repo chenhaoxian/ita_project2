9719fe72bd3467125aba2fdaa4e16ff037332d2c
StarkXiao <xiaojiezhi123@163.com>
2016-07-23 14:06:59 +0800

    add a Logout servlet.

---
+++ Admin/src/com/oocl/ita/starkxiao/project2/admin/login/LogoutServlet.java
@@ -0,0 +1,30 @@
+package com.oocl.ita.starkxiao.project2.admin.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().setAttribute("pass", false);
+		response.sendRedirect("Login");
+	}
+
+}
